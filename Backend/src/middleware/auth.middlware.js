f68e09dc4b6492100bd7eded032a0fa2e71c2f9b
vedant-vijay <vedantrokade159@gmail.com>
2026-03-20 13:40:40 +0530

    implimenting auth.middlware logic

---
+++ Backend/src/middleware/auth.middlware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken"
+
+const ACCsecret = process.env.JWT_SECRET_ACCESS
+
+export const authMiddleware = (req, res, next)=>{
+    const header = req.headers.authorization
+
+    if(!header){
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+    const REFtoken = header.split(" ")[1]
+
+    try{
+        const decoded = jwt.verify(REFtoken, ACCsecret)
+        req.user = decoded
+        next()
+    }
+    catch(err){
+        res.status(401).json({ message: 'Unauthorized' })
+    }
+}
