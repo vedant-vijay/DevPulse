3c8548e2cd20c8a90734c588a683e3113de30761
vedant-vijay <vedantrokade159@gmail.com>
2026-04-01 14:08:24 +0530

    setup the next project as devpulse-frontend and implimented the first login route and understanding the concept of file based routing in the nextjs

---
+++ devpulse-frontend/src/app/login/page.js
@@ -0,0 +1,50 @@
+'use client'
+import { useState } from "react"
+
+function Login(){
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    async function submitHandler(email, password){
+        const response = await fetch("http://localhost:3000/auth/v1/login", {
+            method : "POST",
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            'credentials' : 'include',
+            body : JSON.stringify({email, password})
+        })
+
+        if (!response.ok) {
+            const errorData = await response.json(); 
+            throw new Error(`An error occurred: ${response.status} - ${errorData.message || response.statusText}`);
+            }
+
+            const responseData = await response.json(); 
+            console.log('Success:', responseData);
+            return responseData;
+    }
+
+    return(
+        <form>
+            <input 
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)} 
+                placeholder="enter email here">
+            </input>
+
+            <input 
+                value={password} 
+                onChange={(e)=>setPassword(e.target.value)} 
+                placeholder="enter password here">
+            </input>
+
+            <button onClick={()=>submitHandler(email, password)}>login</button>
+        </form>
+
+        
+        
+    )
+}
+
+export default Login
